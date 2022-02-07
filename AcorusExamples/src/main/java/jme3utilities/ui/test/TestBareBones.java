77ed112b37a9e7ca3696890f2947af5d5e9a5c2b
Stephen Gold <sgold@sonic.net>
2022-02-07 03:28:48 -0800

    AcorusExamples:  add the TestBareBones app

---
+++ AcorusExamples/src/main/java/jme3utilities/ui/test/TestBareBones.java
@@ -0,0 +1,139 @@
+/*
+ Copyright (c) 2022, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.ui.test;
+
+import com.jme3.app.state.AppState;
+import com.jme3.font.Rectangle;
+import com.jme3.renderer.Camera;
+import com.jme3.scene.Node;
+import com.jme3.system.AppSettings;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jme3utilities.Heart;
+import jme3utilities.ui.ActionApplication;
+import jme3utilities.ui.HelpUtils;
+import jme3utilities.ui.InputMode;
+
+/**
+ * Test a bare-bones ActionApplication, one without a FlyByCamera or a
+ * StatsAppState.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class TestBareBones extends ActionApplication {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * message logger for this class
+     */
+    final public static Logger logger
+            = Logger.getLogger(TestBareBones.class.getName());
+    /**
+     * application name (for the title bar of the app's window)
+     */
+    final private static String applicationName
+            = TestBareBones.class.getSimpleName();
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate an ActionApplication without any initial appstates.
+     */
+    private TestBareBones() {
+        super((AppState[]) null);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the TestBareBones application.
+     *
+     * @param ignored array of command-line arguments (not null)
+     */
+    public static void main(String[] ignored) {
+        /*
+         * Mute the chatty loggers in certain packages.
+         */
+        Heart.setLoggingLevels(Level.WARNING);
+        /*
+         * Instantiate the application.
+         */
+        TestBareBones application = new TestBareBones();
+        /*
+         * Customize the window's title bar.
+         */
+        AppSettings settings = new AppSettings(true);
+        settings.setTitle(applicationName);
+
+        settings.setAudioRenderer(null);
+        settings.setGammaCorrection(true);
+        settings.setSamples(4); // anti-aliasing
+        settings.setVSync(true);
+        application.setSettings(settings);
+        /*
+         * Invoke the JME startup code,
+         * which in turn invokes actionInitializeApplication().
+         */
+        application.start();
+    }
+    // *************************************************************************
+    // ActionApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void actionInitializeApplication() {
+        DemoScene.setup(this);
+    }
+
+    /**
+     * Callback invoked immediately after initializing the hotkey bindings of
+     * the default input mode.
+     */
+    @Override
+    public void moreDefaultBindings() {
+        /*
+         * Build and attach the help node.
+         */
+        Camera guiCamera = guiViewPort.getCamera();
+        float x = 10f;
+        float y = guiCamera.getHeight() - 10f;
+        float width = guiCamera.getWidth() - 20f;
+        float height = guiCamera.getHeight() - 20f;
+        Rectangle bounds = new Rectangle(x, y, width, height);
+
+        InputMode dim = getDefaultInputMode();
+        float space = 20f;
+        Node helpNode = HelpUtils.buildNode(dim, bounds, guiFont, space);
+        guiNode.attachChild(helpNode);
+    }
+}
