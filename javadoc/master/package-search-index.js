d42e6611cac5262a77457bff5f9606fc0f44eca4
stephengold <stephengold@users.noreply.github.com>
2026-08-27 19:28:54 +0000

    deploy: 5f7e44faa2923a17565446365432126694b043f5

--- javadoc/master/package-search-index.js
+++ javadoc/master/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jme3utilities.ui"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"jme3utilities.ui"}];updateSearchResults();
